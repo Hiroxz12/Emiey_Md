835efef4e76733edb2bf4fd7a92ffd573bf25c57
Hiroxz12 <110282352+Hiroxz12@users.noreply.github.com>
2022-07-30 23:41:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285730903853', '6282221792667','6285730903853', '6282221792667', '6282135905896'] // Letakan nomor kamu disini
+global.owner = ['6285732267014', '6285890155920','60167495413', '6285325783088', '6285717388502'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
